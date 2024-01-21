7bbbc11c2c1179cacc2808131d9d0f3c1eefcd7c
xuanlinli17 <xuanlinli17@gmail.com>
2024-01-20 23:06:20 -0800

    update pick coke can eval script and upload move near object texture overlay eval script

--- scripts/rt1_pick_coke_can_eval.sh
+++ scripts/rt1_pick_coke_can_eval.sh
@@ -33,9 +33,9 @@ CUDA_VISIBLE_DEVICES=${gpu_id} python real2sim/main_inference.py --policy-model 
   --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
   --env-name GraspSingleOpenedCokeCanInScene-v0 --scene-name google_pick_coke_can_1_v4 \
-  --rgb-overlay-path /home/xuanlin/Real2Sim/ManiSkill2_real2sim/data/real_impainting/google_coke_can_real_eval_2.png \
+  --rgb-overlay-path /home/xuanlin/Real2Sim/ManiSkill2_real2sim/data/real_impainting/google_coke_can_real_eval_1.png \
   --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.35 -0.12 5 --obj-init-y -0.02 0.42 5 \
-  --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0.03 0.03 1 \
+  --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --additional-env-build-kwargs laid_vertically=True;
 
 done
