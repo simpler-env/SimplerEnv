bca1454fb3ad9406fea51c5170f720ebfc5a7bf5
xuanlinli17 <xuanlinli17@gmail.com>
2024-01-27 00:54:06 -0800

    add metric scripts

--- tools/calc_metrics_global.py
+++ tools/calc_metrics_global.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def pearson_correlation_std_discrep(x, y):
+def pearson_correlation(x, y):
     x, y = np.array(x), np.array(y)
     assert x.shape == y.shape
     x = x - np.mean(x)
@@ -9,6 +9,12 @@ def pearson_correlation_std_discrep(x, y):
         pearson = 1
     else:
         pearson = np.sum(x * y) / (np.sqrt(np.sum(x ** 2) * np.sum(y ** 2)) + 1e-8)
+    return pearson
+    
+def pearson_correlation_std_discrep(x, y):
+    x, y = np.array(x), np.array(y)
+    assert x.shape == y.shape
+    pearson = pearson_correlation(x, y)
     discrep = (1 - pearson) * max(np.std(x), np.std(y))
     return discrep
 
@@ -16,6 +22,17 @@ def mean_diff(x, y):
     x, y = np.array(x), np.array(y)
     assert x.shape == y.shape
     return np.abs(np.mean(x) - np.mean(y))
+
+def ranking_violation(x, y):
+    # assuming x is sim result and y is real result
+    x, y = np.array(x), np.array(y)
+    assert x.shape == y.shape
+    rank_violation = 0.0
+    for i in range(len(x) - 1):
+        for j in range(i + 1, len(x)):
+            if (x[i] > x[j]) != (y[i] > y[j]):
+                rank_violation += np.abs(y[i] - y[j])
+    return rank_violation / (len(x) * (len(x) - 1) / 2)
 
 rt_ckpts = ['rt-1-new-best-late', 'rt-1-new-early', 'rt-1-x']
 coke_can_horizontal_sim_success = [1.0, 0.84, 0.80]
@@ -32,12 +49,16 @@ print("coke_can_average_real", coke_can_avg_real)
 
 print("mean_diff(coke_can_horizontal_sim, coke_can_horizontal_real)", mean_diff(coke_can_horizontal_sim_success, coke_can_horizontal_real_success))
 print("pearson_correlation_std_discrep(coke_can_horizontal_sim, coke_can_horizontal_real)", pearson_correlation_std_discrep(coke_can_horizontal_sim_success, coke_can_horizontal_real_success))
+print("ranking_violation(coke_can_horizontal_sim, coke_can_horizontal_real)", ranking_violation(coke_can_horizontal_sim_success, coke_can_horizontal_real_success))
 print("mean_diff(coke_can_vertical_sim, coke_can_vertical_real)", mean_diff(coke_can_vertical_sim_success, coke_can_vertical_real_success))
 print("pearson_correlation_std_discrep(coke_can_vertical_sim, coke_can_vertical_real)", pearson_correlation_std_discrep(coke_can_vertical_sim_success, coke_can_vertical_real_success))
+print("ranking_violation(coke_can_vertical_sim, coke_can_vertical_real)", ranking_violation(coke_can_vertical_sim_success, coke_can_vertical_real_success))
 print("mean_diff(coke_can_standing_sim, coke_can_standing_real)", mean_diff(coke_can_standing_sim_success, coke_can_standing_real_success))
 print("pearson_correlation_std_discrep(coke_can_standing_sim, coke_can_standing_real)", pearson_correlation_std_discrep(coke_can_standing_sim_success, coke_can_standing_real_success))
+print("ranking_violation(coke_can_standing_sim, coke_can_standing_real)", ranking_violation(coke_can_standing_sim_success, coke_can_standing_real_success))
 print("mean_diff(coke_can_avg_sim, coke_can_avg_real)", mean_diff(coke_can_avg_sim, coke_can_avg_real))
 print("pearson_correlation_std_discrep(coke_can_avg_sim, coke_can_avg_real)", pearson_correlation_std_discrep(coke_can_avg_sim, coke_can_avg_real))
+print("ranking_violation(coke_can_avg_sim, coke_can_avg_real)", ranking_violation(coke_can_avg_sim, coke_can_avg_real))
 
 print("=" * 60)
 
@@ -70,11 +91,72 @@ move_near_real_success = [0.633, 0.583, 0.45]
 
 print("mean_diff(move_near_sim_success, move_near_real_success)", mean_diff(move_near_sim_success, move_near_real_success))
 print("pearson_correlation_std_discrep(move_near_sim_success, move_near_real_success)", pearson_correlation_std_discrep(move_near_sim_success, move_near_real_success))
+print("ranking_violation(move_near_sim_success, move_near_real_success)", ranking_violation(move_near_sim_success, move_near_real_success))
 
 print("=" * 60)
 
+# [base success, background, lighting, distractor, table tex, cam pose]
+coke_can_rt1_new_late_sim_table_avg = np.mean(
+    [[0.96, 1.00, 1.00, 0.92, 0.88, 0.04],
+     [0.84, 0.84, 0.92, 0.76, 0.44, 0.08],
+     [0.96, 0.96, 0.96, 0.96, 0.92, 0.04]],
+    axis=0
+)
+coke_can_gengap1234_sim_table_avg = np.mean(
+    [[0.92, 0.80, 0.96, 0.76, 0.92, 0.16],
+     [0.84, 0.56, 0.76, 0.52, 0.52, 0.04],
+     [0.96, 0.76, 0.92, 0.88, 0.72, 0.36]],
+    axis=0
+)
+coke_can_gengap1_sim_table_avg = np.mean(
+    [[0.96, 0.84, 1.00, 0.88, 0.64, 0.20],
+     [0.68, 0.60, 0.76, 0.68, 0.16, 0.00],
+     [0.76, 0.80, 0.52, 0.88, 0.68, 0.12]],
+    axis=0
+)
+
+print("coke_can_rt1_new_late_sim_table_avg", coke_can_rt1_new_late_sim_table_avg)
+print("coke_can_gengap1234_sim_table_avg", coke_can_gengap1234_sim_table_avg)
+print("coke_can_gengap1_sim_table_avg", coke_can_gengap1_sim_table_avg)
+
+coke_can_rt1_sim_factor_diff = np.abs(coke_can_rt1_new_late_sim_table_avg[1:] - coke_can_rt1_new_late_sim_table_avg[0])
+coke_can_gengap1234_sim_factor_diff = np.abs(coke_can_gengap1234_sim_table_avg[1:] - coke_can_gengap1234_sim_table_avg[0])
+coke_can_gengap1_sim_factor_diff = np.abs(coke_can_gengap1_sim_table_avg[1:] - coke_can_gengap1_sim_table_avg[0])
+
+move_near_rt1_new_late_sim_table_avg = np.array([0.467, 0.533, 0.483, 0.600, 0.200, 0.117])
+move_near_gengap1234_sim_table_avg = np.array([0.267, 0.283, 0.300, 0.367, 0.150, 0.100])
+move_near_gengap1_sim_table_avg = np.array([0.383, 0.483, 0.517, 0.467, 0.133, 0.200])
+
+move_near_rt1_sim_factor_diff = np.abs(move_near_rt1_new_late_sim_table_avg[1:] - move_near_rt1_new_late_sim_table_avg[0])
+move_near_gengap1234_sim_factor_diff = np.abs(move_near_gengap1234_sim_table_avg[1:] - move_near_gengap1234_sim_table_avg[0])
+move_near_gengap1_sim_factor_diff = np.abs(move_near_gengap1_sim_table_avg[1:] - move_near_gengap1_sim_table_avg[0])
 
 
+
+
+
+rt1_real_avg = np.array([0.9167, 0.8888, 0.8333, 0.8056, 0.5278, 0.4583])
+gengap1234_real_avg = np.array([0.8333, 0.7222, 0.7500, 0.4444, 0.7500, 0.5000])
+gengap1_real_avg = np.array([0.8333, 0.6667, 0.7917, 0.7500, 0.6667, 0.4583])
+rt1_real_factor_diff = np.abs(rt1_real_avg[1:] - rt1_real_avg[0])
+gengap1234_real_factor_diff = np.abs(gengap1234_real_avg[1:] - gengap1234_real_avg[0])
+gengap1_real_factor_diff = np.abs(gengap1_real_avg[1:] - gengap1_real_avg[0])
+
+print("pearson_correlation(coke_can_rt1_sim_factor_diff, rt1_real_factor_diff)", pearson_correlation(coke_can_rt1_sim_factor_diff, rt1_real_factor_diff))
+print("pearson_correlation(coke_can_gengap1234_sim_factor_diff, gengap1234_real_factor_diff)", pearson_correlation(coke_can_gengap1234_sim_factor_diff, gengap1234_real_factor_diff))
+print("pearson_correlation(coke_can_gengap1_sim_factor_diff, gengap1_real_factor_diff)", pearson_correlation(coke_can_gengap1_sim_factor_diff, gengap1_real_factor_diff))
+print("pearson_correlation(move_near_rt1_sim_factor_diff, rt1_real_factor_diff)", pearson_correlation(move_near_rt1_sim_factor_diff, rt1_real_factor_diff))
+print("pearson_correlation(move_near_gengap1234_sim_factor_diff, gengap1234_real_factor_diff)", pearson_correlation(move_near_gengap1234_sim_factor_diff, gengap1234_real_factor_diff))
+print("pearson_correlation(move_near_gengap1_sim_factor_diff, gengap1_real_factor_diff)", pearson_correlation(move_near_gengap1_sim_factor_diff, gengap1_real_factor_diff))
+
+avg_rt1_sim_factor_diff = np.mean([coke_can_rt1_sim_factor_diff, move_near_rt1_sim_factor_diff], axis=0)
+avg_gengap1234_sim_factor_diff = np.mean([coke_can_gengap1234_sim_factor_diff, move_near_gengap1234_sim_factor_diff], axis=0)
+avg_gengap1_sim_factor_diff = np.mean([coke_can_gengap1_sim_factor_diff, move_near_gengap1_sim_factor_diff], axis=0)
+print("pearson_correlation(avg_rt1_sim_factor_diff, rt1_real_factor_diff)", pearson_correlation(avg_rt1_sim_factor_diff, rt1_real_factor_diff))
+print("pearson_correlation(avg_gengap1234_sim_factor_diff, gengap1234_real_factor_diff)", pearson_correlation(avg_gengap1234_sim_factor_diff, gengap1234_real_factor_diff))
+print("pearson_correlation(avg_gengap1_sim_factor_diff, gengap1_real_factor_diff)", pearson_correlation(avg_gengap1_sim_factor_diff, gengap1_real_factor_diff))
+
+print("=" * 60)
 
 
 
