d04ce22044f82532b1215142b8d4619b429a8b37
Jiayuan-Gu <jigu@eng.ucsd.edu>
2024-01-27 23:34:26 -0800

    Simplify the script to evaluate open/close drawer

--- scripts/rt1_open_drawer_eval.sh
+++ scripts/rt1_open_drawer_eval.sh
@@ -19,24 +19,18 @@ CloseMiddleDrawerCustomInScene-v0
 CloseBottomDrawerCustomInScene-v0
 )
 
+EXTRA_ARGS="--additional-env-build-kwargs shader_dir=rt"
+
 EvalSim() {
   echo ${ckpt_path} ${env_name}
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot google_robot_static --gpu-id ${gpu_id} \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
   --env-name ${env_name} --scene-name frl_apartment_stage_simple \
-  --robot-init-x 0.65 0.85 5 --robot-init-y -0.2 0.2 5 \
+  --robot-init-x 0.65 0.85 3 --robot-init-y -0.2 0.2 3 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0.0 0.0 1 \
-  --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1
-
-# python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-#   --robot google_robot_static --gpu-id ${gpu_id} \
-#   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-#   --env-name ${env_name} --scene-name frl_apartment_stage_simple \
-#   --robot-init-x 0.65 0.85 5 --robot-init-y -0.2 0.2 5 \
-#   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0.0 0.0 1 \
-#   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-#   --additional-env-build-kwargs shader_dir=rt
+  --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
+  ${EXTRA_ARGS}
 }
 
 EvalOverlay() {
@@ -48,7 +42,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.644 0.644 1 --robot-init-y -0.179 -0.179 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.03 -0.03 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_a0.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_a0.png \
+  ${EXTRA_ARGS}
 
 # A1
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
@@ -58,7 +53,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.765 0.765 1 --robot-init-y -0.182 -0.182 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.02 -0.02 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_a1.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_a1.png \
+  ${EXTRA_ARGS}
 
 # A2
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
@@ -68,7 +64,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.889 0.889 1 --robot-init-y -0.203 -0.203 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.06 -0.06 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_a2.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_a2.png \
+  ${EXTRA_ARGS}
 
 # B0
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
@@ -78,7 +75,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.652 0.652 1 --robot-init-y 0.009 0.009 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_b0.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_b0.png \
+  ${EXTRA_ARGS}
 
 # B1
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
@@ -88,7 +86,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.752 0.752 1 --robot-init-y 0.009 0.009 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_b1.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_b1.png \
+  ${EXTRA_ARGS}
 
 # B2
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
@@ -98,7 +97,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.851 0.851 1 --robot-init-y 0.035 0.035 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_b2.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_b2.png \
+  ${EXTRA_ARGS}
 
 # C0
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
@@ -108,7 +108,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.665 0.665 1 --robot-init-y 0.224 0.224 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_c0.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_c0.png \
+  ${EXTRA_ARGS}
 
 # C1
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
@@ -118,7 +119,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.765 0.765 1 --robot-init-y 0.222 0.222 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.025 -0.025 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_c1.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_c1.png \
+  ${EXTRA_ARGS}
 
 # C2
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
@@ -128,7 +130,8 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-x 0.865 0.865 1 --robot-init-y 0.222 0.222 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.025 -0.025 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_c2.png
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_c2.png \
+  ${EXTRA_ARGS}
 }
 
 
