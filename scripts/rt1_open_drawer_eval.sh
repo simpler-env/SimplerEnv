6dfd66129d1bf0255af2216761f3ec37790956de
Jiayuan-Gu <jigu@eng.ucsd.edu>
2024-01-27 17:13:33 -0800

    Update bg image and evaluation scripts

--- scripts/rt1_open_drawer_eval.sh
+++ scripts/rt1_open_drawer_eval.sh
@@ -31,7 +31,7 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 #   --robot google_robot_static --gpu-id ${gpu_id} \
-#   --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
+#   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
 #   --env-name ${env_name} --scene-name frl_apartment_stage_simple \
 #   --robot-init-x 0.65 0.85 5 --robot-init-y -0.2 0.2 5 \
 #   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0.0 0.0 1 \
@@ -59,6 +59,16 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.02 -0.02 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
   --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_a1.png
+
+# A2
+python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
+  --robot google_robot_static --gpu-id ${gpu_id} \
+  --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
+  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --robot-init-x 0.889 0.889 1 --robot-init-y -0.203 -0.203 1 \
+  --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.06 -0.06 1 \
+  --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
+  --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_a2.png
 
 # B0
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
