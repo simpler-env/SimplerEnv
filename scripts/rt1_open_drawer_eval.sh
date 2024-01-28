3cefcc0a9507b1fda588f3203fdb60ac908599df
Jiayuan Gu <jigu@ucsd.edu>
2024-01-28 20:35:08 +0000

    Update open_drawer evaluatoin scripts

--- scripts/rt1_open_drawer_eval.sh
+++ scripts/rt1_open_drawer_eval.sh
@@ -1,8 +1,5 @@
 export MS2_ASSET_DIR=./ManiSkill2_real2sim/data
-
-ckpt_path=./checkpoints/xid77467904_000400120/
-
-gpu_id=0
+export CUDA_VISIBLE_DEVICES=1
 
 declare -a ckpt_paths=(
 "./checkpoints/xid77467904_000400120/"
@@ -15,30 +12,18 @@ OpenTopDrawerCustomInScene-v0
 OpenMiddleDrawerCustomInScene-v0 
 OpenBottomDrawerCustomInScene-v0
 CloseTopDrawerCustomInScene-v0
-CloseMiddleDrawerCustomInScene-v0 
+CloseMiddleDrawerCustomInScene-v0
 CloseBottomDrawerCustomInScene-v0
 )
 
 EXTRA_ARGS="--additional-env-build-kwargs shader_dir=rt"
 
-EvalSim() {
-  echo ${ckpt_path} ${env_name}
-python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
-  --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
-  --robot-init-x 0.65 0.85 3 --robot-init-y -0.2 0.2 3 \
-  --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0.0 0.0 1 \
-  --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-  ${EXTRA_ARGS}
-}
-
 EvalOverlay() {
 # A0
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.644 0.644 1 --robot-init-y -0.179 -0.179 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.03 -0.03 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -47,9 +32,9 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # A1
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.765 0.765 1 --robot-init-y -0.182 -0.182 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.02 -0.02 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -58,9 +43,9 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # A2
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.889 0.889 1 --robot-init-y -0.203 -0.203 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.06 -0.06 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -69,9 +54,9 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # B0
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.652 0.652 1 --robot-init-y 0.009 0.009 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -80,9 +65,9 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # B1
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.752 0.752 1 --robot-init-y 0.009 0.009 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -91,9 +76,9 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # B2
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.851 0.851 1 --robot-init-y 0.035 0.035 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -102,9 +87,9 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # C0
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.665 0.665 1 --robot-init-y 0.224 0.224 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -113,9 +98,9 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # C1
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.765 0.765 1 --robot-init-y 0.222 0.222 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.025 -0.025 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -124,9 +109,9 @@ python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
 
 # C2
 python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static --gpu-id ${gpu_id} \
+  --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-  --env-name ${env_name} --scene-name frl_apartment_stage_simple \
+  --env-name ${env_name} --scene-name dummy \
   --robot-init-x 0.865 0.865 1 --robot-init-y 0.222 0.222 1 \
   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.025 -0.025 1 \
   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
@@ -144,7 +129,7 @@ done
 
 # Debug
 # python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-#   --robot google_robot_static --gpu-id ${gpu_id} \
+#   --robot google_robot_static \
 #   --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
 #   --env-name OpenTopDrawerCustomInScene-v0 --scene-name frl_apartment_stage_simple \
 #   --robot-init-x 0.65 0.75 1 --robot-init-y 0.0 0.0 1 \
@@ -153,7 +138,7 @@ done
 #   --rgb-overlay-path ./ManiSkill2_real2sim/data/real_impainting/open_drawer_b1.png
 
 # python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-#   --robot google_robot_static --gpu-id ${gpu_id} \
+#   --robot google_robot_static \
 #   --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
 #   --env-name CloseBottomDrawerCustomInScene-v0 --scene-name frl_apartment_stage_simple \
 #   --robot-init-x 0.7 0.7 1 --robot-init-y 0.0 0.0 1 \
@@ -161,10 +146,10 @@ done
 #   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1  --enable-raytracing
 
 
-# CUDA_VISIBLE_DEVICES=${gpu_id} python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-#   --robot google_robot_static --gpu-id ${gpu_id} \
+# python real2sim/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
+#   --robot google_robot_static \
 #   --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
-#   --env-name OpenTopDrawerCustomInScene-v0 --scene-name frl_apartment_stage_simple \
+#   --env-name OpenTopDrawerCustomInScene-v0 --scene-name dummy \
 #   --robot-init-x 0.75 0.75 1 --robot-init-y 0.0 0.0 1 \
 #   --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0.0 0.0 1 \
 #   --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1  --enable-raytracing \
